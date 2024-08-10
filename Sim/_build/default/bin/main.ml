989cc3ae402c9acde0341b56d4eee6a787dc9cac
LK672 <144760164+LK672@users.noreply.github.com>
2024-08-11 01:10:19 +0200

    Initial Commit

--- Sim/_build/default/bin/main.ml
+++ Sim/_build/default/bin/main.ml
@@ -6,11 +6,9 @@ module IntOrder : Dict.ORDERED with type t = int = struct
 end
 
 module TreeDict = Dict.RBTDict (IntOrder)
-
 module I = Dict.IRBTDict (TreeDict)
 
 let d = I.empty ()
 let () = I.insert (5, "hi") d
-
 
 let () = print_endline "Success"
